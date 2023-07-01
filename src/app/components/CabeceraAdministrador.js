6bd7089c466c4506fc3b1d9278a57a4bee6ee47b
Andy Chero <andicht01@gmail.com>
2023-06-30 21:23:36 -0500

    pequeños cambios

--- src/app/components/CabeceraAdministrador.js
+++ src/app/components/CabeceraAdministrador.js
@@ -1,13 +1,16 @@
 import { ObtenerFechaHoraActual } from '../../utils/utils'
 import { Timer } from '../modules/layout/Timer'
 import logoEmpresa from '../../assets/css/images/Logo.jpeg'
+import { Login } from '../pages/Login'
 export const CabeceraAdministrador = ()=>{
     return(
         <>
             <header className='w-full h-[30%] '>
                 <div className="flex">
                     <div className='w-[20%] py-2 flex justify-center items-center'>
-                        <img src={logoEmpresa} className='rounded-xl w-[80%] h-[80%]'></img>
+                        <a href='/Login' className='cursor-pointer'>
+                            <img src={logoEmpresa} className='rounded-xl w-[80%] h-[80%]'></img>
+                        </a>
                     </div>
                     <div className='w-[30%] flex justify-center items-center'>
                         <label className='text-3xl font-medium  text-white'>{ObtenerFechaHoraActual()}</label>
