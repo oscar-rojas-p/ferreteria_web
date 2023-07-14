9501f9f24c4517bfe382aea9894edbb4d3112ee7
oscar-rojas <oscar.rojas.p@uni.pe>
2023-07-14 00:10:00 -0500

    carrito de compras dinamico y otras modificaciones

--- src/app/pages/Catalogo.js
+++ src/app/pages/Catalogo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Cabecera} from '../components/Cabecera';
 
 import imagen1 from '../../assets/css/images/Alicate de corte.jpg';
@@ -12,11 +12,35 @@ import imagen8 from '../../assets/css/images/descarga.jpg';
 import imagen9 from '../../assets/css/images/Destornillador.jpg';
 import imagen10 from '../../assets/css/images/Kit de alicates.jpg';
 
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import generico from '../../assets/css/images/generico.jpg';
+import interrogacion from '../../assets/css/images/pregunta.jpg';
+import { useConexBD } from '../../hooks/useConexBD';
+
 // import MiCarousel from '../components/carousel';
 // import BodyList from '../components/listasBody';
 // import Blog from '../components/blog'
+
 export const Catalogo = () => {
+
+    const {listarProductos,productos} = useConexBD()
     const categorias =[{title :'Cemento'},{title :'Herramientas'},{title :'Hierro'},{title :'Pinturas'},{title :'Disolventes'},{title :'PVC'}, {title :'Tanques'},{title :'Otros'}]
+
+    const [costoTotal,setCostoTotal] = useState(0)
+
+    const [open, setOpen] = useState(false);
+
+    
+    useEffect(() => {
+        listarProductos()
+    },[])
+
     return(
         <>
             <Cabecera/>
@@ -39,88 +63,57 @@ export const Catalogo = () => {
                         <div className=''>
                             <p className='break-words text-blue-700 text-justify'>¿Tienes interés en un producto que no está en nuestro catálogo? <p className='text-blue-900 font-semibold'>¡Queremos saberlo!</p> <p className='font-semibold text-orange-500 cursor-pointer'>hablar con nuestros asesores</p></p>
                         </div>
-                        <div style={{color:'black',width:'100%',border:'1px solid black',marginTop:'50px'}}>
-                                <div style={{width:'100%',height:'50px',textAlign:'center',color:'white',background:'#d48051',fontSize:'30px'}}>Tu Carrito</div>
-                                <div style={{width:'100%',background:'#f1f1f1',padding:'30px'}}>
-                                    <div style={{fontSize:'20px'}}>Tu Total: S/200.00</div>
-                                    <div style={{width:'100%',marginTop:'20px',height:'80px',borderRadius:'5px',background:'black',padding:'25px 20px 25px 20px'}}>
-                                        <p style={{color:'white',fontSize:'25px',cursor:'pointer'}}>Pagar <i style={{float:'right'}} className="fa fa-shopping-basket" aria-hidden="true"></i></p>
+                        <div className='text-black w-full border border-black mt-12'>
+                                <div className='w-full h-12 text-center text-white text-3xl bg-[#d48051]'>Tu Carrito</div>
+                                <div className='w-full bg-[#f1f1f1] p-8'>
+                                    <div className='text-xl'>Tu Total: S/{costoTotal}</div>
+
+                                    <div onClick={() => setOpen(true)} className='w-full mt-5 h-20 rounded-md cursor-pointer bg-black px-6 py-5'>
+                                        <p className='text-white text-2xl'>Pagar <i className="float-right fa fa-shopping-basket" aria-hidden="true"></i></p>
                                     </div>
                                 </div>
                         </div>
                     </div>
                 </div>
-                <div className='w-[75%]' style={{}}>
-                    <div style={{width:'100%'}}>
-                        <div style={{display:'flex',width:'100%'}}>
-                            <div style={{width:'33%',textAlign:'center',padding:'20px'}}>
-                                <div style={{border:'1px solid #888484',boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px',padding:'20px',borderRadius:'20px'}}>
-                                    <img style={{margin:'0 auto'}} src={imagen1} />
-                                    <div className="hover:scale-110" style={{color:'black',textAlign:'center',width:'100%',fontSize:'30px'}}><button style={{border:'1px solid black',padding:'5px 20px 5px 20px',borderRadius:'15px',background:'#d48051',color:'white'}}>Alicate de corte</button></div>
-                                </div>
-                            </div>
-                            <div style={{width:'33%',textAlign:'center',padding:'20px'}}>
-                                <div style={{border:'1px solid #888484',boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px',padding:'20px',borderRadius:'20px'}}>
-                                    <img style={{margin:'0 auto'}} src={imagen2} />
-                                    <div className="hover:scale-110" style={{color:'black',textAlign:'center',width:'100%',fontSize:'30px'}}><button style={{border:'1px solid black',padding:'5px 20px 5px 20px',borderRadius:'15px',background:'#d48051',color:'white'}}>Alicate</button></div>
-                                </div>
-                            </div>
-                            <div style={{width:'33%',textAlign:'center',padding:'20px'}}>
-                                <div style={{border:'1px solid #888484',boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px',padding:'20px',borderRadius:'20px'}}>
-                                    <img style={{margin:'0 auto',height:'222px'}} src={imagen3} />
-                                    <div className="hover:scale-110" style={{color:'black',textAlign:'center',width:'100%',fontSize:'30px'}}><button style={{border:'1px solid black',padding:'5px 20px 5px 20px',borderRadius:'15px',background:'#d48051',color:'white'}}>Cable mellizo blanco</button></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div style={{display:'flex',width:'100%'}}>
-                            <div style={{width:'33%',textAlign:'center',padding:'20px'}}>
-                                <div style={{border:'1px solid #888484',boxShadow:'box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px',padding:'20px',borderRadius:'20px'}}>
-                                    <img style={{margin:'0 auto'}} src={imagen4} />
-                                    <div className="hover:scale-110" style={{color:'black',textAlign:'center',width:'100%',fontSize:'30px'}}><button style={{border:'1px solid black',padding:'5px 20px 5px 20px',borderRadius:'15px',background:'#d48051',color:'white'}}>Cable mellizo verde</button></div>
-                                </div>
-
-                            </div>
-                            <div style={{width:'33%',textAlign:'center',padding:'20px'}}>
-                                <div style={{border:'1px solid #888484',boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px',padding:'20px',borderRadius:'20px'}}>
-                                    <img style={{margin:'0 auto'}} src={imagen5} />
-                                    <div className="hover:scale-110" style={{color:'black',textAlign:'center',width:'100%',fontSize:'30px'}}><button style={{border:'1px solid black',padding:'5px 20px 5px 20px',borderRadius:'15px',background:'#d48051',color:'white'}}>Canaleta 15x10x2MT</button></div>
-                                </div>
-
-                            </div>
-                            <div style={{width:'33%',textAlign:'center',padding:'20px'}}>
-                                <div style={{border:'1px solid #888484',boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px',padding:'20px',borderRadius:'20px'}}>
-                                    <img style={{margin:'0 auto'}} src={imagen6} />
-                                    <div className="hover:scale-110" style={{color:'black',textAlign:'center',width:'100%',fontSize:'30px'}}><button style={{border:'1px solid black',padding:'5px 20px 5px 20px',borderRadius:'15px',background:'#d48051',color:'white'}}>Cinta aislante trupper</button></div>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div style={{display:'flex',width:'100%'}}>
-                            <div style={{width:'33%',textAlign:'center',padding:'20px'}}>
-                                <div style={{border:'1px solid #888484',boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px',padding:'20px',borderRadius:'20px'}}>
-                                    <img style={{margin:'0 auto',height:'222px'}} src={imagen7} />
-                                    <div className="hover:scale-110" style={{color:'black',textAlign:'center',width:'100%',fontSize:'30px'}}><button style={{border:'1px solid black',padding:'5px 20px 5px 20px',borderRadius:'15px',background:'#d48051',color:'white'}}>Clavos de acero</button></div>
-                                </div>
-
-                            </div>
-                            <div style={{width:'33%',textAlign:'center',padding:'20px'}}>
-                                <div style={{border:'1px solid #888484',boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px',padding:'20px',borderRadius:'20px'}}>
-                                    <img style={{margin:'0 auto'}} src={imagen8} />
-                                    <div className="hover:scale-110" style={{color:'black',textAlign:'center',width:'100%',fontSize:'30px'}}><button style={{border:'1px solid black',padding:'5px 20px 5px 20px',borderRadius:'15px',background:'#d48051',color:'white'}}>Tuerca</button></div>
-                                </div>
-
-                            </div>
-                            <div style={{width:'33%',textAlign:'center',padding:'20px'}}>
-                                <div style={{border:'1px solid #888484',boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px',padding:'20px',borderRadius:'20px'}}>
-                                    <img style={{margin:'0 auto'}} src={imagen9} />
-                                    <div className="hover:scale-110" style={{color:'black',textAlign:'center',width:'100%',fontSize:'30px'}}><button style={{border:'1px solid black',padding:'5px 20px 5px 20px',borderRadius:'15px',background:'#d48051',color:'white'}}>Destornillador</button></div>
-                                </div>
-
-                            </div>
+                <div className='w-[75%]'>
+                    <div className='w-full'>
+                        <div className='grid grid-cols-3 gap-4 w-full'>
+                            {
+                                productos.map(function(data,i){
+                                    return  (
+                                        <div className='border border-[#888484] rounded-2xl p-5' >
+                                            <div className='w-full text-black text-center pb-4 font-medium text-3xl'>{(data.nomProducto).toUpperCase()}</div>
+                                            <img className='m-auto pb-5' src={generico} />
+                                            <div onClick={() => setCostoTotal(costoTotal+data.precioVenta)} className="hover:scale-110 text-black text-center w-[90%] m-auto"><button className='border border-black px-6 w-full py-2 rounded-xl bg-[#d48051] text-white text-lg'>S/. {data.precioVenta}</button></div>
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                     </div>
                 </div>   
             </div>
+
+            <Dialog
+                    open={open}
+                    onClose={() => setOpen(false)}
+            >   
+                <DialogTitle id="alert-dialog-title">
+                    <label className='text-2xl  w-full'><b>Confirmar Compra</b></label>
+                </DialogTitle>
+
+                <DialogContent>
+                    <div className='flex'>
+                        <div className='w-44 text-center'>Esta seguro de realizar la compra?</div>
+                        <img src={interrogacion} className='w-16'></img>
+                    </div>
+                </DialogContent>
+                
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)}>Cancelar</Button>
+                    <Button onClick={{}} >Confirmar</Button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }  
